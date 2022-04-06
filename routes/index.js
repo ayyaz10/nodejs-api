711b3d92787b9f24145f08db11c162d68bacda23
ayyaz10 <codementee@outlook.com>
2022-04-07 02:00:49 +0500

    server files

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const cors = require('cors');
+// const app = express();
+
+// router.get('/', function(req, res) {
+//   console.log('working')
+//   res.send('helo')
+// })
+
+
+// router.post('/emaildata', function(req, res) {
+//   console.log('helo')
+//   console.log(req.body);
+//   res.json("hi");
+// });
+
+
+module.exports = router;
